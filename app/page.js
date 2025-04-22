7bd9a7d0c17d03f7beed009b504cce2ed424a01b
DavBenBen <108869224+DavBenBen@users.noreply.github.com>
2025-04-22 11:35:22 +0200

    Refactor localStorage handling and clean up JSX structure in Home component

--- app/page.js
+++ app/page.js
@@ -12,15 +12,19 @@ export default function Home() {
 
   // Load `todos` from localStorage on the client side
   useEffect(() => {
-    const savedTodos = localStorage.getItem("todos");
-    if (savedTodos) {
-      setTodos(JSON.parse(savedTodos));
+    if (typeof window !== "undefined") {
+      const savedTodos = localStorage.getItem("todos");
+      if (savedTodos) {
+        setTodos(JSON.parse(savedTodos));
+      }
     }
   }, []);
 
   // Save `todos` to localStorage whenever it changes
   useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
+    if (typeof window !== "undefined") {
+      localStorage.setItem("todos", JSON.stringify(todos));
+    }
   }, [todos]);
 
   const handleAddTask = (taskText) => {
@@ -41,34 +45,23 @@ export default function Home() {
   };
 
   return (
-    <>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Nunito:ital,wght@0,200..1000;1,200..1000&family=Raleway:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-
-      <div className="flex items-center justify-center h-screen">
-        <div className="join w-1/2 h-4/6">
-          {/* Left Side */}
-          <div className="flex flex-col space-y-4 w-1/2 h-fill p-10 shadow-md border-2 border-base-200 rounded-l-xl">
-            <div className="flex flex-col justify-center items-center h-full">
-              <Timer initialTime={timeMax} />
-              <TimeSelector timeMax={timeMax} setTimeMax={setTimeMax} />
-            </div>
+    <div className="flex items-center justify-center h-screen">
+      <div className="join w-1/2 h-4/6">
+        {/* Left Side */}
+        <div className="flex flex-col space-y-4 w-1/2 h-fill p-10 shadow-md border-2 border-base-200 rounded-l-xl">
+          <div className="flex flex-col justify-center items-center h-full">
+            <Timer initialTime={timeMax} />
+            <TimeSelector timeMax={timeMax} setTimeMax={setTimeMax} />
           </div>
-          {/* Right Side */}
-          <div className="flex flex-col space-y-4 w-1/2 h-fill p-10 shadow-md border-2 border-base-200 rounded-r-xl">
-            <div className="flex flex-col justify-center items-center h-fit">
-              <TaskMaker onAddTask={handleAddTask} />
-              <TodoList todos={todos} onDelete={handleDelete} onToggle={handleToggle} />
-            </div>
+        </div>
+        {/* Right Side */}
+        <div className="flex flex-col space-y-4 w-1/2 h-fill p-10 shadow-md border-2 border-base-200 rounded-r-xl">
+          <div className="flex flex-col justify-center items-center h-fit">
+            <TaskMaker onAddTask={handleAddTask} />
+            <TodoList todos={todos} onDelete={handleDelete} onToggle={handleToggle} />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
